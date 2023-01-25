fdce0bf58ac65b49e134eed5570d9ed588f95ca2
DamianTobiasz <damian.tobiasz@lowgular.io>
2023-01-25 20:57:35 +0100

    feature: header with navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,36 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from './components/home/home.component';
-import {CategoryComponent} from './components/category/category.component';
-import {StoreComponent} from './components/store/store.component';
-import {HomeComponentModule} from './components/home/home.component-module';
-import {CategoryComponentModule} from './components/category/category.component-module';
-import {StoreComponentModule} from './components/store/store.component-module';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CategoryComponent } from './components/category/category.component';
+import { StoreComponent } from './components/store/store.component';
+import { HomeComponentModule } from './components/home/home.component-module';
+import { CategoryComponentModule } from './components/category/category.component-module';
+import { StoreComponentModule } from './components/store/store.component-module';
+import { CategoriesServiceModule } from './services/categories.service-module';
 
 const routes: Routes = [
   {
-    path: '', component:
-    HomeComponent
+    path: '',
+    component: HomeComponent,
   },
   {
     path: 'categories/:categoryId',
-    component: CategoryComponent
+    component: CategoryComponent,
   },
   {
     path: 'stores/:storeId',
-    component: StoreComponent
-  }
+    component: StoreComponent,
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), HomeComponentModule, CategoryComponentModule, StoreComponentModule],
+  imports: [
+    RouterModule.forRoot(routes),
+    HomeComponentModule,
+    CategoryComponentModule,
+    StoreComponentModule,
+    CategoriesServiceModule,
+  ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,29 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { CategoriesService } from '../../services/categories.service';
+import { CategoryQueryModel } from '../../query-models/category.query-model';
 
 @Component({
   selector: 'app-home',
   styleUrls: ['./home.component.scss'],
   templateUrl: './home.component.html',
   encapsulation: ViewEncapsulation.Emulated,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
+  readonly categories$: Observable<CategoryQueryModel[]> =
+    this._categoriesService.getAll().pipe(
+      map((categories) =>
+        categories.map((category) => ({
+          name: category.name,
+          link: `categories/${category.id}`,
+        }))
+      )
+    );
+
+  constructor(private _categoriesService: CategoriesService) {}
 }
