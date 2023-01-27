691125d19cd2bfaac2276a200a4b80336cb84db6
DamianTobiasz <damian.tobiasz@lowgular.io>
2023-01-27 20:42:15 +0100

    feature: stores on home page

---
+++ src/app/components/stores/stores.component.ts
@@ -0,0 +1,32 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import { InMemoryStoresStorage } from '../../storages/stores/in-memory-stores.storage';
+import { Observable } from 'rxjs';
+import { StoreQueryModel } from '../../query-models/store.query-model';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-stores',
+  templateUrl: './stores.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class StoresComponent {
+  readonly stores$: Observable<StoreQueryModel[]> = this._storesStorage
+    .select()
+    .pipe(
+      map((stores) =>
+        stores.map((store) => ({
+          name: store.name,
+          link: `categories/${store.id}`,
+          logoUrl: store.logoUrl.slice(1),
+          distanceInKilometers: store.distanceInMeters * 0.001,
+        }))
+      )
+    );
+
+  constructor(private _storesStorage: InMemoryStoresStorage) {}
+}
--- src/app/query-models/store.query-model.ts
+++ src/app/query-models/store.query-model.ts
@@ -1,4 +1,6 @@
 export interface StoreQueryModel {
   readonly name: string;
   readonly link: string;
+  readonly logoUrl: string;
+  readonly distanceInKilometers: number;
 }
